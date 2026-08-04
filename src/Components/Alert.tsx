1cc66cdf058038fbc794e9b130f2e52104288432
Pat W <pat.woods10@gmail.com>
2026-08-04 13:07:20 -0400

    feat: small new Alert test component

---
+++ src/Components/Alert.tsx
@@ -0,0 +1,13 @@
+import {ReactNode} from 'react'
+
+interface Props {
+	children: ReactNode;
+}
+
+function Alert({children}: Props) {
+	return (
+		<div className="alert alert-primary">{children}</div>
+	)
+}
+
+export default Alert
